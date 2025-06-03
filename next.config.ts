efb8220426c86c6e996904bf897fc420c93b6ed4
Thiago da Silva <s_th@live.com>
2025-06-03 16:38:10 -0300

    Configurações para deploy no git pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    domains: [
-      "temporeal.lance.com.br",
-      // adicione outros domínios que usar aqui se necessário
-    ],
+    unoptimized: true, // Desativa otimização de imagens para exportação
   },
+  basePath: "/live-match-feed", // importante para GitHub Pages
+  assetPrefix: "/live-match-feed", // garante que assets carreguem do path certo
 };
 
 module.exports = nextConfig;
